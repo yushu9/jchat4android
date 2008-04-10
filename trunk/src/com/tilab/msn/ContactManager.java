72444461feff2c69b2ab7f37d03ac8c65f8e03b4
ughetti <ughetti@9eb1904f-8b52-0410-a7a3-9996f2c7a580>
2008-04-10 14:50:55 +0000

    bug fixing on ContactManager better exception handling in MsnAgent and related behaviours

--- trunk/src/com/tilab/msn/ContactManager.java
+++ trunk/src/com/tilab/msn/ContactManager.java
@@ -7,17 +7,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import android.app.Activity;
 import android.database.Cursor;
 import android.location.Location;
-import android.os.SystemProperties;
 import android.provider.Contacts.People;
 
 public class ContactManager {
 
-	
+
 	private static ContactManager manager = null;
 	//The key of this map is the local name (phone number)
 	private Map<String, Contact> otherContactsMap; 
@@ -25,76 +23,76 @@ public class ContactManager {
 	private Contact myContact;
 	private final Logger myLogger = Logger.getMyLogger(this.getClass().getName());
 	private boolean updateOngoing = false;
-	
-	
+
+
 	public boolean updateIsOngoing(){
 		return updateOngoing;
 	}
-	
+
 	public void setOngoingUpdate() {
 		updateOngoing = true;
 	}
-	
+
 	private ContactManager() {
 		updateOngoing = false; 
 		otherContactsMap = new HashMap <String, Contact>();
-		
-		
-		
+
+
+
 		//FIXME: Try a better way to retrieve MyContact name
 		myContact = new Contact("Me",true);
-		
+
 	}
 
 	public void readPhoneContacts(Activity act){
 		//perform a query on contacts database returning all contacts data in name ascending order
 		Cursor c = act.getContentResolver().query(People.CONTENT_URI, null, null, null, People.NAME + " DESC");
-		
+
 		act.startManagingCursor(c);
-		
+
 		int nameCol = c.getColumnIndex(People.NAME);
 		int numtelCol = c.getColumnIndex(People.NUMBER);
-		
+
 		//Let's get contacts data
 		if (c.first()){
 			do {
-				
+
 				String numTel = c.getString(numtelCol);
 				String name = c.getString(nameCol);
-				
+
 				myLogger.log(Logger.INFO, "Found contact "+ name + " with numtel " + numTel);
-				
+
 				Contact cont = new Contact(name, true);
 				otherContactsMap.put(numTel, cont);
-				
+
 			} while(c.next());
 		}
 	}
-	
-	
-	
+
+
+
 	//This methods adds or updates a contact 
 	public void addOnlineContact(AID agentAid, Location loc){
-			
+
 		synchronized (otherContactsMap) {
 			//Is the contact already there?
 			Contact cont = otherContactsMap.get(agentAid.getLocalName());
-			
+
 			//If not create a new one
 			if (cont == null){
 				cont = new Contact(agentAid.getLocalName(), false);
-				otherContactsMap.put(agentAid.getName(), cont);
+				otherContactsMap.put(agentAid.getLocalName(), cont);
 			} 
-			
+
 			cont.setAgentContact(agentAid.getName());
 			cont.setLocation(loc);
 		}	
 	}
-	
+
 	public void setOffline(AID agentId) {
 		synchronized (otherContactsMap) {
 			Contact c  = otherContactsMap.get(agentId.getLocalName());
-			
+
 			//If a contact is local (It's in the phone contatcs) it must be shown as offline
 			//If the contact is not local, remove it when it goes offline
 			if (c.isLocal()){
@@ -104,51 +102,51 @@ public class ContactManager {
 			}
 		}
 	}
-	
-	
-	
+
+
+
 	//Agent Id is the AID.getLocalName()
 	public Contact getContactByAgentId(String agentId){
-		
+
 		Contact c;
-		
+
 		synchronized (otherContactsMap) {
 			c= otherContactsMap.get(agentId);
 		}
-		
+
 		return c;
 	}
-	
+
 	public Contact getMyContact() {
 		return myContact;
 	}
-	
+
 	public static ContactManager getInstance() {
 
 		synchronized (ContactManager.class) {
 			if (manager == null){
 				manager = new ContactManager();
 			}
-			
+
 		}
-		
+
 		return manager;
 	}
 
-	
-	
+
+
 	//We cannot modify the contacts from this list, we copy th list to avoid race conditions
 	public List<Contact> getOtherContactList() {
-		
+
 		ArrayList<Contact> list;
-		
+
 		synchronized (otherContactsMap) {
 			list = new ArrayList<Contact>(otherContactsMap.values());
 		}		
-		
+
 		return list;
 	}
-		
-	
+
+
 }
 
--- trunk/src/com/tilab/msn/MsnAgent.java
+++ trunk/src/com/tilab/msn/MsnAgent.java
@@ -1,13 +1,5 @@
 package com.tilab.msn;
 
-import java.util.Calendar;
-import java.util.Iterator;
-
-import android.app.Activity;
-import android.app.Notification;
-import android.content.Context;
-import android.location.Location;
-
 import jade.core.AID;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
@@ -20,6 +12,7 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.util.Logger;
 import jade.wrapper.gateway.GatewayAgent;
+import android.location.Location;
 
 
 public class MsnAgent extends GatewayAgent {
@@ -32,15 +25,15 @@ public class MsnAgent extends GatewayAgent {
 	public static final String PROPERTY_NAME_LOCATION_ALT="Altitude";
 
 	public static final String CHAT_ONTOLOGY= "chat_ontology";
-	
+
 	private DFAgentDescription myDescription;
 	private ACLMessage subscriptionMessage;
 	private ContactsUpdaterBehaviour contactsUpdaterB;
 	private MessageReceiverBehaviour messageRecvB;
-	
-	
+
+
 	private final Logger myLogger = Logger.getMyLogger(this.getClass().getName());
-	
+
 	//In this method we shall register to df and subscribe
 	protected void setup() {
 		super.setup();
@@ -56,7 +49,7 @@ public class MsnAgent extends GatewayAgent {
 		subscriptionMessage = DFService.createSubscriptionMessage(this, this.getDefaultDF(), myDescription, null);
 
 		Location curLoc = ContactManager.getInstance().getMyContact().getLocation();
-		
+
 		Property p = new Property(PROPERTY_NAME_LOCATION_LAT,new Double(curLoc.getLatitude()));
 		msnServiceDescription.addProperties(p);
 		p = new Property(PROPERTY_NAME_LOCATION_LONG,new Double(curLoc.getLongitude()));
@@ -64,7 +57,7 @@ public class MsnAgent extends GatewayAgent {
 		p= new Property(PROPERTY_NAME_LOCATION_ALT,new Double(curLoc.getAltitude()));
 		msnServiceDescription.addProperties(p);		
 		myDescription.setName(this.getAID());
-		
+
 		try {
 			myLogger.log(Logger.INFO, "Registering to DF!");
 			DFService.register(this, myDescription);
@@ -72,7 +65,7 @@ public class MsnAgent extends GatewayAgent {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		//added behaviour to dispatch chat messages
 		messageRecvB = new MessageReceiverBehaviour(); 
 		addBehaviour(messageRecvB);
@@ -81,26 +74,26 @@ public class MsnAgent extends GatewayAgent {
 		contactsUpdaterB = new ContactsUpdaterBehaviour(Long.parseLong(args[0]));
 		addBehaviour(contactsUpdaterB);
 	}
-	
+
 	public ACLMessage getSubscriptionMessage(){
 		return subscriptionMessage;
 	}
-	
+
 	public DFAgentDescription getAgentDescription(){
 		return myDescription;
 	}
-	
 
-	
+
+
 	//Here we will deregister and unsubscribe
 	protected void takeDown() {
 		myLogger.log(Logger.INFO, "Starting agent takeDown() ");
-		
+
 		ACLMessage unsubcribeMsg = DFService.createCancelMessage(this, getDefaultDF(), getSubscriptionMessage());
 		send(unsubcribeMsg);
 
 		myLogger.log(Logger.INFO, "DS Subscription Canceling message was sent!");
-		
+
 		try {
 			DFService.deregister(this, myDescription);
 			myLogger.log(Logger.INFO, "Deregistering from DF!");
@@ -109,10 +102,10 @@ public class MsnAgent extends GatewayAgent {
 			e.printStackTrace(); 
 		}	
 	}
-	
+
 	//used to pass data to agent
 	protected void processCommand(final Object command) {
-		
+
 		if (command instanceof SynchCommandBehaviour) {
 			addBehaviour( (Behaviour) command);
 		}else if (command instanceof Behaviour){
@@ -123,67 +116,74 @@ public class MsnAgent extends GatewayAgent {
 			releaseCommand(command);
 		} 
 	}
-	
+
 	private class MessageReceiverBehaviour extends CyclicBehaviour{
 
-	
-		
+
+
 		public void action() {
-			MessageTemplate mt = MessageTemplate.MatchOntology(CHAT_ONTOLOGY);
-			ACLMessage msg = myAgent.receive(mt);
-			//If a message is received
-			if(msg != null){
-				myLogger.log(Logger.INFO, msg.toString());
-						
-				//retrieve the session id
-				String sessionId = msg.getConversationId();
-				myLogger.log(Logger.INFO, "Received Message... session ID is " + sessionId);
-				
-				//check if there's an activity to update
-				ContactsUIUpdater updater = MsnSessionManager.getInstance().retrieveMsgReceivedUpdater(sessionId);
-				Contact sender = ContactManager.getInstance().getContactByAgentId(msg.getSender().getLocalName());
-				MsnSessionMessage sessionMessage = new MsnSessionMessage(msg.getContent(),sender.getName(),true);
-				
-				//If we have no activity we need to add a notification
-				if (updater == null) {
-					
-					myLogger.log(Logger.INFO, "Updater not found... creating a new session object");
-					
-					//Before adding the notification w have to add a new session
-					MsnSession session = MsnSessionManager.getInstance().createNewMsnSession(sessionId);
-					//Add all participants
-			
-					Contact myContact = ContactManager.getInstance().getMyContact();
-					myLogger.log(Logger.INFO, "Adding sender " + sender.getName() + "as a session participant");
-					session.addParticipant(sender);
-					for (jade.util.leap.Iterator it = msg.getAllReceiver(); it.hasNext();) {
-						 AID agentId = (AID) it.next();
-						 
-						 if (!agentId.getName().startsWith(myContact.getAgentContact())){
-							 Contact otherContact = ContactManager.getInstance().getContactByAgentId(agentId.getLocalName());
-							 myLogger.log(Logger.INFO, "Adding contact " + otherContact.getName() + "as a session participant");
-							 session.addParticipant(otherContact);
-						 }
+
+			try {
+				MessageTemplate mt = MessageTemplate.MatchOntology(CHAT_ONTOLOGY);
+				ACLMessage msg = myAgent.receive(mt);
+				//If a message is received
+				if(msg != null){
+					myLogger.log(Logger.INFO, msg.toString());
+
+					//retrieve the session id
+					String sessionId = msg.getConversationId();
+					myLogger.log(Logger.INFO, "Received Message... session ID is " + sessionId);
+
+					//check if there's an activity to update
+					ContactsUIUpdater updater = MsnSessionManager.getInstance().retrieveMsgReceivedUpdater(sessionId);
+					Contact sender = ContactManager.getInstance().getContactByAgentId(msg.getSender().getLocalName());
+					MsnSessionMessage sessionMessage = new MsnSessionMessage(msg.getContent(),sender.getName(),true);
+
+					//If we have no activity we need to add a notification
+					if (updater == null) {
+
+						myLogger.log(Logger.INFO, "Updater not found... creating a new session object");
+
+						//Before adding the notification w have to add a new session
+						MsnSession session = MsnSessionManager.getInstance().createNewMsnSession(sessionId);
+						//Add all participants
+
+						Contact myContact = ContactManager.getInstance().getMyContact();
+						myLogger.log(Logger.INFO, "Adding sender " + sender.getName() + "as a session participant");
+						session.addParticipant(sender);
+						for (jade.util.leap.Iterator it = msg.getAllReceiver(); it.hasNext();) {
+							AID agentId = (AID) it.next();
+
+							if (!agentId.getName().startsWith(myContact.getAgentContact())){
+								Contact otherContact = ContactManager.getInstance().getContactByAgentId(agentId.getLocalName());
+								myLogger.log(Logger.INFO, "Adding contact " + otherContact.getName() + "as a session participant");
+								session.addParticipant(otherContact);
+							}
+						}
+
+						session.addMessage(sessionMessage);
+						IncomingNotificationUpdater notificationUpdater = MsnSessionManager.getInstance().getNotificationUpdater();
+						myLogger.log(Logger.INFO, "Calling notification updater to add a new notification on status bar");
+						notificationUpdater.postMessageNotification(msg);
+					} else {
+						myLogger.log(Logger.INFO, "Updater found... retrieving existing session");
+						MsnSession session = MsnSessionManager.getInstance().retrieveSession(sessionId);
+						session.addMessage(sessionMessage);
+						myLogger.log(Logger.INFO, "Posting UI update on the retrieved updater");
+						updater.postUIUpdate(sessionMessage);
 					}
-					
-					session.addMessage(sessionMessage);
-					IncomingNotificationUpdater notificationUpdater = MsnSessionManager.getInstance().getNotificationUpdater();
-					myLogger.log(Logger.INFO, "Calling notification updater to add a new notification on status bar");
-					notificationUpdater.postMessageNotification(msg);
-				} else {
-					myLogger.log(Logger.INFO, "Updater found... retrieving existing session");
-					MsnSession session = MsnSessionManager.getInstance().retrieveSession(sessionId);
-					session.addMessage(sessionMessage);
-					myLogger.log(Logger.INFO, "Posting UI update on the retrieved updater");
-					updater.postUIUpdate(sessionMessage);
-				}
-				
-			}else{
-				block();
+
+				}else{
+					block();
+				}				
 			}
+			catch(Throwable t) {
+				myLogger.log(Logger.SEVERE,"***  Uncaught Exception for agent " + myAgent.getLocalName() + "  ***",t);
+			}
+
 		}
 	}
-	
-	
+
+
 }
 
