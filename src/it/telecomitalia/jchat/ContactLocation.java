db5f5f8d94166b19cc26a9bb14452cec1862602a
stefano.semeria <stefano.semeria@9eb1904f-8b52-0410-a7a3-9996f2c7a580>
2008-07-29 08:37:14 +0000

    Added LGPL header to all source files

--- src/it/telecomitalia/jchat/ContactLocation.java
+++ src/it/telecomitalia/jchat/ContactLocation.java
@@ -1,3 +1,25 @@
+/*****************************************************************
+ jChat is a  chat application for Android based on JADE
+  Copyright (C) 2008 Telecomitalia S.p.A. 
+ 
+ GNU Lesser General Public License
+
+ This is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation, 
+ version 2.1 of the License. 
+
+ This software is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this software; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA  02111-1307, USA.
+ *****************************************************************/
+
 package it.telecomitalia.jchat;
 
 import android.location.Location;
